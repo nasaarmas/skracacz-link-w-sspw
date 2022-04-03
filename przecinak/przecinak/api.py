f9beea2b6e7ea8144f0b8451907d1bfb54ffa941
Michał Rokita <mrokita@mrokita.pl>
2022-04-03 16:43:37 +0200

    feat: add login example

--- przecinak/przecinak/api.py
+++ przecinak/przecinak/api.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBasic
+from starlette import status
 
 app = FastAPI()
 
 
+auth = HTTPBasic()
+
 @app.get(f'/')
-def index():
-    return 'Hello'
+def index(auth = Depends(auth)):
+    print(auth)
+    if (auth.username != 'mrokita2'):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return 'Hello 5'
